41678c76e4fd4e3bfe90f8658ba74f7eaaa5dff4
aysun gunduz <113923715+aysungunduz@users.noreply.github.com>
2023-07-04 12:40:42 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+
+const app = express();
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html")
+})
+
+app.post("/", function (req, res) {
+    let firstName = req.body.fName;
+    let lastName = req.body.lName;
+    let email = req.body.email;
+
+    console.log(firstName, lastName, email);
+})
+
+app.listen(3000, function (req, res) {
+    console.log("Server is running on port 3000.")
+})
